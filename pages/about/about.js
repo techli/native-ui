3aa226714a495511770f3b602ff21c714e5b9931
lixiaohua <lixiaohua01@chinatopcredit.com>
2021-06-02 07:48:47 +0800

    第一次提交

---
+++ pages/about/about.js
@@ -0,0 +1,54 @@
+//about.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    logosrc:'../../image/logo.png',
+    adrimg:'../../image/icon-address.png',
+    clockimg: '../../image/icon-clock.png',
+    comment: '../../image/聊天.png',
+    phoneimg: '../../image/icon-phone.png',
+    jtimg: '../../image/icon-jt.png',
+    picimg: '../../image/icon-pic.png',
+    imgalist: ['../../image/微信.png'],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+  calling: function () {
+    wx.makePhoneCall({
+      phoneNumber: '15138244113', 
+    })
+  },
+  preViewWX: function () {
+    wx.previewImage({
+      current : 'https://t7.baidu.com/it/u=1819248061,230866778&fm=193&f=GIF',
+      urls : ['https://t7.baidu.com/it/u=1819248061,230866778&fm=193&f=GIF']
+    })
+  },
+  getLocation: function (){
+    wx.openLocation({
+      latitude: 33.479216,
+      longitude: 115.089585,
+      name:"李竹园回收站",
+      address:"沈丘县白集镇李竹园村",
+      scale: 28
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,42 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    appointment: '../../image/预约.png',
+    recycllist: '../../image/回收列表.png',
+    contactUs: '../../image/联系我们.png',
+    shopImg: '../../image/二手商品.png',
+    signupimg:'../../image/signup.png',
+    imgsrc:'https://ss0.bdstatic.com/9bA1vGfa2gU2pMbfm9GUKT-w/timg?searchbox_feed&quality=120&wh_rate=0&size=f648_364&ref=http%3A%2F%2Fwww.baidu.com&sec=0&di=1619440b1bc5580ece1e18c556a408d7&src=http%3A%2F%2Ft12.baidu.com%2Fit%2Fu%3D2318927072%2C2551034291%26fm%3D175%26s%3DF9E09B544C2154095A6098DF030090F1%26w%3D660%26h%3D370%26img.JPEG',
+    iconsrc:'../../image/usercount.png',
+    jtsrc:'../../image/icon-jt.png',
+    imgUrls: [
+      '../../image/轮播图1.png',
+      '../../image/轮播图2.png',
+      '../../image/轮播图3.png',
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000 
+  },
+  onShareAppMessage: function (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '教育模板首页',
+      path: '/pages/index/index',
+      success: function (res) {
+        // 转发成功
+        console.log("转发成功")
+      },
+      fail: function (res) {
+        // 转发失败
+        onsole.log("转发失败")
+      }
+    }
+  }
+})
